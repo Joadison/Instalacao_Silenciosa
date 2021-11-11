f3b8eb88cc5bea89c319679c926f12224bf11120
Joadison <32674418+Joadison@users.noreply.github.com>
2021-11-11 12:40:25 -0300

    Update Instalador.bat

--- Instalador.bat
+++ Instalador.bat
@@ -15,18 +15,18 @@ echo  INSTALANDO PROGRAMAS
 echo ----------------------
 echo .
 echo === Desinstalar Kaspersky ====
-setup_kes.exe /pKLLOGIN=KLAdmin /pKLPASSWD=@kaspersky_tjce /s /x
-msiexec.exe /x {7EC66A9F-0A49-4DC0-A9E8-460333EA8013} KLPASSWD=@kaspersky_tjce /qn
-msiexec.exe /x {32A3A4F4-B792-11D6-A78A-00B0D0170510} /qn
+setup_kes.exe  /s /x
+msiexec.exe /x {}  /qn
+msiexec.exe /x {} /qn
 echo .
 echo === Enviado CPA Assinador ====
-xcopy "\\FCBDEINF2153660\WinCDP\Programas\Assinador_CPA.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" /Y
+xcopy "" "" /Y
 echo .
 echo === Enviado o Arquivo UVNC ====
-xcopy "\\FCBDEINF2153660\WinCDP\Programas\UltraVNC.ini" "C:\Program Files\uvnc bvba\UltraVNC" /-Y
+xcopy "" "" /-Y
 echo .
 echo  === Movendo AnyDesk  ===
-xcopy "\\FCBDEINF2153660\WinCDP\Programas\AnyDesk.exe" "C:\Users\Public\Desktop" /Y
+xcopy "" "" /Y
 echo .
 echo === CRIANDO PASTA SCAN ===
 MD "C:\Scan"
@@ -34,16 +34,13 @@ icacls "C:\scan" /grant Todos:(OI)(CI)F /T
 net share scan="C:\scan"
 echo.
 echo === Instalando JAVA ====
-\\FCBDEINF2153660\WinCDP\Programas\jre-8u281-windows-x64.exe /s
+jre-8u281-windows-x64.exe /s
 echo .
 echo === Instalando Kaspersky ====
-\\FCBDEINF2153660\WinCDP\Programas\installer.exe  /s
+installer.exe  /s
 echo .
 echo === Instalando Itom ====
-\\FCBDEINF2153660\WinCDP\Programas\TJITOM_FORCE.exe
-echo .
-echo  === SAJPG === 
-C:\Saj\sajpg5\sajpg5.exe
+TJITOM_FORCE.exe
 echo .
 echo .
 echo .
